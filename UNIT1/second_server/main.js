076f4974b88f4123ab629b9ebe715cc37f39fc24
Igarashi Hiroto <HirotoIgarashi@gmail.com>
2019-10-13 12:30:26 +0900

    update

--- UNIT1/second_server/main.js
+++ UNIT1/second_server/main.js
@@ -3,23 +3,27 @@ const port = 3000,
   http = require('http'),
   httpStatus = require('http-status-codes'),
   // requestとresponseのパラメータを指定してサーバを作成
-  app = http.createServer((request, response) => {
-    // 「リクエストを受信しました！」
-    console.log('Received an incoming request!');
-    // クライアントに対するレスポンスを書く
-    response.writeHead(httpStatus.OK, {
-      "Content-Type": "text/html"
-    });
+  app = http.createServer();
 
-    let responseMessage = '<H1>Hello, Universe!</h1>';
-    response.write(responseMessage);
-    response.end();
-    // 「レスポンスを受信しました：」
-    console.log(`Sent a response : ${responseMessage}`);
+app.on('request', (request, response) => {
+
+  // リクエストデータをログに出す
+  console.log(request.method);
+  console.log(request.url);
+  console.log(request.headers);
+
+  // レスポンスを準備
+  response.writeHead(httpStatus.OK, {
+    "Content-Type": "text/html"
   });
+
+  let responseMessage = '<H1>This will show on the screen.</h1>';
+  // HTMLでレスポンスする
+  response.end(responseMessage);
+});
 
 // アプリケーションのサーバーにポート3000を監視させる
 app.listen(port);
 
-// 「サーバーが起動して、このポートを監視中：」
+// 「サーバーが起動しました、監視しているポート番号は：」
 console.log(`The server has started and is listening on port number: ${port}`);
